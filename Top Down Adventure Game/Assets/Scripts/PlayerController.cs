551d009cc00b897c7fdd8d9306accf5f7ace0036
aaquinox <145797278+aaquinox@users.noreply.github.com>
2023-10-06 13:51:27 -0400

    added a key and player can collect it, also added a new scene

--- Top Down Adventure Game/Assets/Scripts/PlayerController.cs
+++ Top Down Adventure Game/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement; //importing SceneManagement library
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.5f;
+    public bool hasKey = false;
+    public GameObject key;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,6 +53,14 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("hit");
             SceneManager.LoadScene(1); //access SceneManager class for Loadscene function
+        }
+
+        if (collision.gameObject.tag.Equals("key"))
+        {
+            Debug.Log("obtained key");
+            key.SetActive(false);  //key disappears
+
+            hasKey = true;
         }
     }
 
